f94b7cb78538c13f8913c7b81a4445f42e0654d5
zakytaufiq <151290835+kytaufiq@users.noreply.github.com>
2025-05-18 20:33:06 +0700

    Update about.php

--- prakweb/about.php
+++ prakweb/about.php
@@ -2,10 +2,8 @@
 session_start();
 $username = "";
 if(isset($_SESSION['username'])){
-    // sudah login ,set variable username dengan data yang sudah ada di session
     $username = $_SESSION['username'];
 }else{
-    // belum login ,maka redirect ke halaman login
     header('Location:index.php');
 }
 
