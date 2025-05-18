d570cc789adddc34e3e8bf87ec38da23f6cd1a62
zakytaufiq <151290835+kytaufiq@users.noreply.github.com>
2025-05-18 20:33:50 +0700

    Update signup.php

--- prakweb/signup.php
+++ prakweb/signup.php
@@ -7,7 +7,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $username = $_POST['username'];
     $password = $_POST['password'];
 
-    // Cek apakah username sudah digunakan
     $query_check = "SELECT * FROM users WHERE username = '$username'";
     $result_check = mysqli_query($conn, $query_check);
 
@@ -15,7 +14,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         echo '<script>alert("Username sudah digunakan. Silakan pilih username lain."); window.location.href = "index.php";</script>';
         exit();
     } else {
-        // Simpan user baru
         $query_signup = "INSERT INTO users (username, password, role) VALUES ('$username', '$password', 'user')";
         $result_signup = mysqli_query($conn, $query_signup);
 
